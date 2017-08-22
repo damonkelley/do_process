599e8833823e954264b3ab24a9a3a9dd30215306
Damon Kelley <damon.kelley@gmail.com>
2017-08-21 22:03:07 -0500

    Create an umbrella application

--- mix.exs
+++ mix.exs
@@ -1,27 +1,16 @@
-defmodule DoProcess.Mixfile do
+defmodule DoProcess.System.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :do_process,
-     version: "0.1.0",
-     elixir: "~> 1.5",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases(),
-     deps: deps()]
-  end
-
-  def application do
-    [extra_applications: [:logger],
-     mod: {DoProcess.Application, []}]
+    [
+      apps_path: "apps",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   defp deps do
     [{:credo, "~> 0.8", only: [:dev, :test], runtime: false},
     {:dialyxir, "~> 0.5", only: [:dev], runtime: false}]
-  end
-
-  defp aliases do
-    [test: "test --no-start --trace"]
   end
 end
